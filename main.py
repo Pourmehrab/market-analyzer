c72ef2e1fcd40910fdbb1f9ab58ab743a1996e36
Mahmoud <pourmehrab@gmail.com>
2019-02-25 22:03:26 -0500

    problem computing outstanding shares from balance sheet

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from ticker import Ticker
 
-dataSP500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+def pull_sp500_tickers():
+    raw_data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 
 
-ticker = Ticker("ATVI")
-q = ticker.get_quote()
-p = ticker.get_market_price()
-ticker.describe()
-print("done!")
+if __name__ == "__main__":
+
+    ticker = Ticker("atvi")
+    ticker.describe()
+
+    print("done!")
--- ticker.py
+++ ticker.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import json
 import multiprocessing
 import pandas as pd
@@ -15,14 +16,18 @@ class Ticker():
         self.b_s = self.get_financial_rec("balance-sheet")
         self.c_f = self.get_financial_rec("cash-flow")
 
+        self.shares = self.compute_outstanding_shares()
+
+        print('done')
+
     def describe(self):
-        print("{:s}:".format(self.ticker))
-        print("Market cap is {:s} (>100 M).".format(
-            self.quote["Market Cap"], self.ticker))
-        print("For every {:>1.1f} (4-15) spent buying, $1 will be profited in a year.".format(
-            self.quote["PE Ratio (TTM)"]))
-        print("Dividend Yield is {:>s} (>7%).".format(
-            self.quote["Forward Dividend & Yield"], self.ticker))
+        print(
+            "{:>5s} ==============================================================================".format(self.ticker))
+        print("Market Cap is {:s} (>100 M).".format(self.quote["Market Cap"], self.ticker))
+
+        print("For every $X (4-15) spent buying a stock, I should receive $1 in profit one year later as an owner.")
+
+        print("Dividend Yield is {:>s} (>7%).".format(self.quote["Forward Dividend & Yield"], self.ticker))
 
         print('done')
 
@@ -33,6 +38,44 @@ class Ticker():
         return si.get_quote_table(self.ticker, dict_result=True)
 
     def get_financial_rec(self, doc_type):
+        '''
+        All numbers in thousands $[[
+
+        :param doc_type:
+        :return:
+        '''
+
         document = pd.read_html(
             'https://finance.yahoo.com/quote/{:s}/{:s}?p={:s}'.format(self.ticker, doc_type, self.ticker))
-        return document
+
+        df = document[0].dropna().replace("-", 0).T
+
+        df.columns = df.iloc[0]
+        df = df.drop(df.index[0])
+
+        col_names = list(df)
+
+        annual_doc = df.rename(columns={col_names[0]: 'Date', })
+
+        convert_type = {c: 'int64' for c in list(df)}
+        convert_type['Date'] = pd.datetime
+        del convert_type[col_names[0]]
+
+        annual_doc = annual_doc.astype(convert_type)
+
+        return annual_doc
+
+    def compute_outstanding_shares(self):
+        df = self.b_s
+        return df["Preferred Stock"] + df["Common Stock"] - df["Treasury Stock"]
+
+    def compute_eps(self):
+        '''
+        Earnings per Share: This is probably the most important number to understand. This is the earnings per 1 share
+        or the profit for 1 share. To calculate this number, divide the company’s total net income by the common shares
+        outstanding.
+
+        :return:
+        '''
+        # eps = self.i_s.loc['Net Income Applicable To Common Shares'] / df.loc['b']
+        pass
